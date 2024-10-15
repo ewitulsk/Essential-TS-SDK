63c2c29acdf7424df584fa037dc8351dd57dcca9
Evan Witulski <ewitulsk@calpoly.edu>
2024-10-15 01:13:34 -0700

    init

---
+++ lib.ts
@@ -0,0 +1,52 @@
+import { context } from 'fetch-h2'
+
+
+const { fetch, disconnect, disconnectAll, onPush } = context({httpProtocol: "http2"});
+
+//Could probably turn into a 1 onliner, but thats too much brain power
+function makeUrlOptions(option_keys: any[], option_vals: any[]){
+    let url = ""
+    if(option_keys.length != option_vals.length){
+        throw Error("URL options must be same length")
+    }
+
+    console.log(option_keys.length)
+
+    if(option_keys.length > 0){
+        url += "?"
+        let i = 0;
+        while(i < option_keys.length){
+            let key = option_keys[i]
+            let val = option_vals[i]
+            url+=`${key}=${val}`
+            i+=1
+        }
+
+    }
+    return url
+}
+
+export class EssentialClient {
+    baseUrl: String;
+
+    constructor(baseURL: string) {
+        // Initialize the URL object
+        this.baseUrl = baseURL
+    }
+
+    async getContracts(contract_addr: String) {
+        return await(await fetch(this.baseUrl+`/get-contract/${contract_addr}`)).json()
+    }
+
+    async listContracts(page: number=0, start: number = 0, end: number | null=null) {
+        let keys = ["start", "page"]
+        let vals = [start, page]
+        if(end){
+            keys.push("end")
+            vals.push(end)
+        }
+        let req = await fetch(this.baseUrl+`/list-contracts`+makeUrlOptions(keys, vals))
+        return await(req).json()
+    }
+}
+
